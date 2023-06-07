676390c1333d43d5916ab3d9d2fa394c3b1e27e7
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-06-07 09:25:47 -0400

    set up Express app to use Pug as the template engine

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const express = require("express"); const path = require("path");
 const app = express();
 const port = process.env.PORT || "8888";
 
+//set up Express app to use Pug as the template engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
 //test message
 app.get("/", (req, res) => {
     res.status(200).send("Test page");
