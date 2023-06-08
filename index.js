b64a79a586522d429901786c8f6ad97a83fe97d8
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-06-08 17:14:15 -0400

    mongodb connected

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 //import required modules
 const express = require("express"); const path = require("path");
+const dotenv = require("dotenv");
+
+dotenv.config(); //load our custom enironment variables
+
 
 //set up Express object and port
 const app = express();
@@ -7,8 +11,8 @@ const port = process.env.PORT || "8888";
 
 //MongoDB stuff
 const { MongoClient } = require("mongodb");
-const dbUrl = "mongodb://localhost:27017/testdb"; //default port is 27017
-// const dbUrl = "mongodb+srv://<username>:<password>@cluster0.9s8pywv.mongodb.net/?retryWrites=true&w=majority";
+const { get } = require("express/lib/response");
+const dbUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.DB_HOST}/?retryWrites=true&w=majority`;
 const client = new MongoClient(dbUrl);
 
 //set up Express app to use Pug as the template engine
@@ -19,19 +23,46 @@ app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
 
 //rendering specific views/pages
-app.get("/", (req, res) => {
-    res.render("index", { title: "Home" });
+app.get("/", async (req, res) => {
+    let links = await getLinks();
+    res.render("index", { title: "Home", menu: links });
 });
 
-app.get("/about", (req, res) => {
-    res.render("about", {title: "About"});
+app.get("/about", async (req, res) => {
+    let links = await getLinks();
+    res.render("about", {title: "About", menu: links });
 });
 
-app.get("/collection", (req, res) => {
-    res.render("collection", {title: "Collection"});
+app.get("/collection", async (req, res) => {
+    let links = await getLinks();
+    let collectionImg = await getCollectionImg();
+    res.render("collection", {
+        title: "Collection",
+        menu: links,
+        img: collectionImg
+    });
 });
 
 //set up server listening
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`);
 });
+
+//MONGO HELPER FUNCTIONS
+async function connection() {
+    await client.connect();
+    db = client.db("testdb"); return db;
+}
+
+async function getLinks() {
+    db = await connection();
+    var results = db.collection("menuLinks").find({});
+    res = await results.toArray();
+    return res;
+}
+
+async function getCollectionImg() {
+    var results = db.collection("collectionImg").find({});
+    res = await results.toArray();
+    return res;
+}
